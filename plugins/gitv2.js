c2d04a3a53debe6b418d49b12dd641c2fdd03a71
ɾɑʍՏ հɑժ. Ƙ <97965450+Ottapettavan@users.noreply.github.com>
2022-03-27 07:57:16 +0400

    Update gitv2.js

--- plugins/gitv2.js
+++ plugins/gitv2.js
@@ -1,20 +1,20 @@
-//modified @ chunkindepadayali (liza mwol™)©
+//modified @ ottapettavan (shah mwol™)©
 
-const Lizamwol = require('../events');
+const Shahmwol = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Lizamwol.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+Shahmwol.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:LIZA MWOL [OWNER]\n' // full name
-            + 'ORG:ʟɪᴢᴀ ᴍᴡᴏʟ™;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=918075379950:+91 8137829228\n' // WhatsApp ID + phone number
-            + 'TEL;type=CELL;type=VOICE;waid= '+Config.NU+' :+91 8075379950\n' // 
+            + 'FN:SHAH MWOL [OWNER]\n' // full name
+            + 'ORG:ѕнaн ᴍᴡᴏʟ™;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=917736951082:+994406368079\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid= '+Config.NU+' :+917736951082\n' // 
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "LIZA MWOL [OWNER]", vcard: vcard}, MessageType.contact, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "ʟɪᴢᴀ ᴍᴡᴏʟ™", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('liza mwol v2.jpg')}}}});
+await message.client.sendMessage(message.jid,{displayname: "SHAH MWOL [OWNER]", vcard: vcard}, MessageType.contact, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "ѕнaн ᴍᴡᴏʟ™", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('SHAMOWL.jpg')}}}});
 }))
