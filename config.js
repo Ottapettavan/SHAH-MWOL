aabd672a64bb08248ca64b0e8ed2665302c49600
ɾɑʍՏ հɑժ. Ƙ <97965450+Ottapettavan@users.noreply.github.com>
2024-02-15 20:43:03 +0400

    Update config.js SHAH-MWOL
    Signed-off-by: ɾɑʍՏ հɑժ. Ƙ <97965450+Ottapettavan@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,90 +1,60 @@
-/* Copyright (C) 2020 Yusuf Usta.
+const fs = require('fs')
+const chalk = require('chalk')
 
-WhatsAsena - Yusuf Usta
-*/
+//contact details
+global.ownernumber = ['917736951082']
+global.ownername = "OTTᗩᑭETTᗩᐯᗩᑎ"//owner name
+global.ytname = "YT: OttapettavanRKBotz.inc"
+global.socialm = "GitHub: Ottapettavan"
+global.location = "Indian"
 
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+global.botname = '𝐒𝐇𝐀𝐇 𝐌𝐖𝐎𝐋' //name of the bot
 
-// Özel Fonksiyonlarımız
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+//sticker details
+global.stickername = 'OTTᗩᑭETTᗩᐯᗩᑎ'
+global.packname = 'Sticker By'
+global.author = 'SHAHMWOL Bot'
+//console view/theme
+global.themeemoji = '🧩'
+global.wm = "OttapettavanRK botz inc."
+
+//theme link
+global.link = 'https://chat.whatsapp.com/F7p4E5x9972EcD9w6pAPYg'
+
+//custom prefix
+global.prefa = ['!']
+
+//false=disable and true=enable
+global.welcome = false //auto welcome
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
+global.anti212 = true //auto block +212
+global.autoread_status = false //auto view status/story
+
+
+
+//reply messages
+global.mess = {
+    done: '*here you go!* \n\n*🧞SHAHMWOL Bot 🧚*\n\n*🧩 Bot link:* \nhttps://github.com/Ottapettavan/SHAH-MWOL\n',
+    prem: '*This feature can be used by premium user only*',
+    admin: '*This feature can be used by admin only*',
+    botAdmin: '*This feature can only be used when the bot is a group admin* ',
+    owner: '*This feature can be used by owner only*',
+    group: '*This feature is only for groups*',
+    private: '*This feature is only for private chats*',
+    wait: '*In process...* ',    
+    error: '*Error!*',
 }
 
-DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
-DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+global.thumb = fs.readFileSync('./Gallery/SHAMOWL.jpg')
 
-module.exports = {
-    VERSION: 'v8.0.0',
-    CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.SHAH_SESSION === undefined ? '' : process.env.SHAH_SESSION,
-    ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
-    AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
-    GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://i.hizliresim.com/loUtAb.jpg' : process.env.GAN_IMAGE,
-
-    PM_BLOCK: process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK,
-    LANG: process.env.LANGUAGE === undefined ? 'TR' : process.env.LANGUAGE.toUpperCase(),
-    ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
-    KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
-    BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
-    WELCOME: process.env.WELCOME === undefined ? 'pp' : process.env.WELCOME,
-    OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
-    ALL: process.env.ALL_CAPTION === undefined ? 'Made By SHAH MWOL' : process.env.ALL_CAPTION,
-    MENTION: process.env.TAG_REPLY === undefined ? '918921483992@s.whatsapp.net' : process.env.TAG_REPLY,
-    ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
-    PLKS: process.env.THERI_LIST === undefined ? false : process.env.THERI_LIST,
-    MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
-    BGMFILTER: process.env.BGM_FILTER === undefined ? false : convertToBool(process.env.BGM_FILTER),
-    DISBGM: process.env.DISABLE_JID_BGM_FILTER === undefined ? false : process.env.DISABLE_JID_BGM_FILTER,
-    STICKERP: process.env.AUTO_STICKER === undefined ? true : convertToBool(process.env.AUTO_STICKER),
-    DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
-    BOTSHAH: process.env.BOT_NAME === undefined ? '  𝐒𝐇𝐀𝐇 𝐌𝐖𝐎𝐋࿐  \n\n   *ωнαтsαρρ вσт™*   \n' : process.env.BOT_NAME,
-    NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
-    THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
-    SONGD: process.env.SONGD === undefined ? '✮⃝🕊️▁ ▂ ▄ ▅ ▆ ▇ █ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿𝙄𝙉𝙂 𝙎𝙊𝙉𝙂 █ ▇ ▆ ▅ ▄ ▂ ▁✮⃝🕊️' : process.env.SONGD,
-    SONGU: process.env.SONGU === undefined ? '✮⃝🕊️·✩·.¸¸.·¯⍣✩ UPLOADING SONG ✩⍣¯·.¸¸.·✩·.¸..✮⃝🕊️' : process.env.SONGU,
-    FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
-    BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
-    UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
-    UNMUTEMSG: process.env.UNMUTE_MESSAGE === undefined ? 'default' : process.env.UNMUTE_MESSAGE,
-    GEAR: process.env.CHANGE_BGM_TO === undefined ? 'one' : process.env.CHANGE_BGM_TO,
-    WORKTYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
-    PROMOTEMSG: process.env.PROMOTE_MESSAGE === undefined ? 'default' : process.env.PROMOTE_MESSAGE,
-    DEMOTEMSG: process.env.DEMOTE_MESSAGE === undefined ? 'default' : process.env.DEMOTE_MESSAGE,
-    BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
-    AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
-    WEL_GIF: process.env.WEL_GIF === undefined ? 'https://i.imgur.com/nErXUGj.mp4' : process.env.WEL_GIF,
-    GIF_BYE: process.env.GIF_BYE === undefined ? 'https://i.imgur.com/Z1jCYGN.mp4' : process.env.GIF_BYE,
-    HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
-    TAGPLK: process.env.TAG_HEADER === undefined ? 'Note this' : process.env.TAG_HEADER,
-    SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
-    YAK: process.env.YAK === undefined ? '918921483992,0' : process.env.YAK,
-    OWNERSHIP: process.env.OWNER_SHIP === undefined ? 'SHAH-MWOL' : process.env.OWNER_SHIP,
-    SHAH: process.env.LOGO_LINK === undefined ? 'https://imgur.com/sueftdB.jpg' : process.env.LOGO_LINK,
-    MWOL: process.env.BGM_DURATION === undefined ? '7280542' : process.env.BGM_DURATION,
-    BOTV2: process.env.BOTV2_NAME === undefined ? 'ꪶ͢ɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃ⃝ѕнaнᴍᴏʟ࿐' : process.env.BOTV2_NAME,
-    SHAH_AI: process.env.SHAH_AI === undefined ? 'false' : process.env.SHAH_AI,
-    ALIVEBUTTON: process.env.ALIVEBUTTON === undefined ? 'ʜᴇʟʟᴏ ʙʀᴏ' : process.env.ALIVEBUTTON,
-    ALIVE_BUTTON: process.env.ALIVE_BUTTON === undefined ? 'ʜɪ ᴅᴜᴅᴇ' : process.env.ALIVE_BUTTON,
-    LG_LOGO: process.env.LG_LOGO === undefined ? 'https://imgur.com/sueftdB.jpg' : process.env.LG_LOGO,
-    LOGO_NAME: process.env.LOGO_NAME === undefined ? 'ꪶ͢ɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃ⃝ѕнaнᴍᴏʟ࿐' : process.env.LOGO_NAME,
-    NOLOG: process.env.NO_LOG === undefined ? 'true' : process.env.NO_LOG,
-    NU: process.env.NUMBER === undefined ? false : process.env.NUMBER,
-    BRANCH: 'master',
-    HEROKU: {
-        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
-        API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
-        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
-    },
-    DATABASE_URL: DATABASE_URL,
-    DATABASE: DATABASE_URL === './whatsasena.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
-    RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
-    NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? false : process.env.SUDO,
-    DEBUG: DEBUG,
-    WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
-    SUPPORT: "918075379950-1634134075",
-    SUPPORT2: "905511384572-1617736751",
-    SUPPORT3: "905511384572-1621015274"
-};
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
